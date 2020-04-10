2f5b146e4b456ec9567ae2eff96a548d4bddb6e4
Carlos Revilla <clro26.k@gmail.com>
2020-04-10 23:17:17 +0200

    registrarse con correo

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { MicuentaComponent } from './container/micuenta/micuenta.component';
 import { RegistrarseComponent } from './container/registrarse/registrarse.component';
 import { HomeComponent } from './components/home/home.component';
+import { ConfirmedComponent } from './container/user/confirmed/confirmed.component';
 
 
 const routes: Routes = [
--- frontend/src/app/container/user/confirmed/confirmed.component.ts
+++ frontend/src/app/container/user/confirmed/confirmed.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-confirmed',
   templateUrl: './confirmed.component.html',
-  styleUrls: ['./confirmed.component.scss']
+  styleUrls: []
 })
 export class ConfirmedComponent implements OnInit {
 
-  constructor() { }
+  email:string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public userService: UserService ) { }
 
   ngOnInit(): void {
+    const token = this.route.snapshot.params.token;
+    this.userService.getUserInfo(token)
+    .subscribe(
+      res => this.userService.setUser(res)
+    )
+    localStorage.setItem('authToken', token);
+    setTimeout(() => {
+      this.router.navigate(['']);
+    }, 3000);
   }
 
 }
